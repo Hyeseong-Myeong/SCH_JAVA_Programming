3e699a51f5ec564eafa6cb13e254b5728d199285
Hyeseong-int <a01025819869@gmail.com>
2019-04-12 20:37:55 +0900

    HW3 commit

---
+++ HW/HW3/src/RPStest.java
@@ -0,0 +1,16 @@
+
+public class RPStest {
+	public static void main(String[] args)	{
+		String usr, com;
+		String res;
+		RPS rps = new RPS();
+		
+		com = "rock";
+		rps.setCom(com);
+		
+		usr = "scissors";
+		res = rps.checkCom(usr);
+		
+		System.out.println(res + ", usr-" + usr + ", com-" + com);
+	}
+}
